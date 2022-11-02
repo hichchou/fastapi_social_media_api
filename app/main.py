690c58a460ea7bc054319fb904e5c4cad2fa654a
hichchou <hamza.ichchou@gmail.com>
2022-11-02 16:00:07 +0100

    added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
@@ -8,6 +10,23 @@ from .config import settings
 #models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# select domains allowed to access API
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "https://www.google.com"
+# ]
+#allow all domains (wild card)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
